4bfb4f863d8c96974d48ad616afc7690b095b662
Anzzas <blackmindeu@gmail.com>
2025-03-17 12:30:08 +0100

    Added Snake::move()

--- Snake/snake.cpp
+++ Snake/snake.cpp
@@ -2,5 +2,26 @@
 
 void Snake::move(Direction direction)
 {
+	Position newHeadPos{m_body.front()};
 
+	switch (direction)
+	{
+	case Direction::up:
+		newHeadPos.y -= 1;
+		break;
+	case Direction::right:
+		newHeadPos.x += 1;
+		break;
+	case Direction::down:
+		newHeadPos.y += 1;
+		break;
+	case Direction::left:
+		newHeadPos.x -= 1;
+		break;
+	}
+
+	for (size_t i = m_body.size() - 1; i > 0; i--)
+		m_body[i] = m_body[i - 1];
+
+	m_body[0] = newHeadPos;
 }
--- Snake/snake.h
+++ Snake/snake.h
@@ -1,6 +1,7 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 #include "common.h"
+#include <deque>
 
 class Snake
 {
@@ -10,6 +11,13 @@ public:
 	void grow();
 
 private:
+
+	std::deque<Position> m_body{
+	Position{0, 0},
+	Position{0, 1},
+	Position{0, 2},
+	Position{0, 3},
+	};
 
 	Direction m_direction{};
 };
